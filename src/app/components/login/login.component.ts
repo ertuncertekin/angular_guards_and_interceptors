77fe5765639d6c849921714c00e34234d5db6110
ertuncertekin <ertekinertunc@gmail.com>
2024-06-03 14:32:05 +0300

    Form Edit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { Router, RouterLink } from '@angular/router';
-import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,22 +12,28 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     NavbarComponent,
     RouterLink,
+    ReactiveFormsModule
     //HttpClientModule burası eziyor
   ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  constructor(private _http: HttpClient, private router: Router) { }
-
-  getApi() {
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private _http: HttpClient, private router: Router) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  postApi() {
     // Token'ı test amaçlı localStorage'a ayarlayalım
     localStorage.setItem('token', "denemetoken123");
     this.router.navigate(["/loading"])
     setTimeout(() => {
       this.router.navigate([""]);
     }, 3000); // 3 saniye beklettik. //
-    this._http.post("https://jsonplaceholder.typicode.com/posts", {}).subscribe(res => {
+    this._http.post("https://jsonplaceholder.typicode.com/posts", this.loginForm.value).subscribe(res => {
       console.log(res);
     });
   }
